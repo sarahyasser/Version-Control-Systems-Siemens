ebef615b6192d1357e2f92276fbefe83bd5fcdb1
sarahyasser <sarahyasserxo@gmail.com>
2023-10-30 15:29:51 +0200

    getAverage Function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 #include <vector>
 using namespace std;
-int getSum(vector<int> vec)
+
+int getSum(vector<int> &vec)
 {
     int sum=0;
     vector<int>::iterator iter = vec.begin();
@@ -11,8 +12,16 @@ int getSum(vector<int> vec)
     }
     return sum;
 }
+double getAverage(vector<int> &vec)
+{
+    if (vec.empty())
+        return 0;
+    int sum = getSum(vec);
+    return (double) sum / vec.size();
+}
 int main() {
     vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
-     cout<< getSum(vec);
+    cout << "Sum: "<< getSum(vec) << endl;
+    cout << "Average: " << getAverage(vec) << endl;
     return 0;
 }
