ba6bc08b0231187e9f63be2087177d65f0660db2
sarahyasser <sarahyasserxo@gmail.com>
2023-10-30 15:36:33 +0200

    getMinimum Function

--- main.cpp
+++ main.cpp
@@ -11,8 +11,20 @@ int getSum(vector<int> vec)
     }
     return sum;
 }
+int getMinimum(vector<int> &vec) {
+    int min = INT_MAX;
+
+    vector<int>::iterator iter = vec.begin();
+    for(iter; iter< vec.end(); iter++){
+        if (*iter < min) {
+            min = *iter;
+        }
+    }
+    return min;
+}
 int main() {
     vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
-     cout<< getSum(vec);
+    cout<< getSum(vec);
+    cout << "Minimum: " << getMinimum(vec) << endl;
     return 0;
 }
